9cfa114a8dee82aeb0371e4ce7f8c680b6e9f80c
Wall-e <you@mail.com>
2023-10-02 22:54:45 +0300

    Решение задачи 27

---
+++ Task02(27)/Program.cs
@@ -0,0 +1,22 @@
+﻿/* На ввод: число num
+на вывод: сума цифр числа num  */
+
+int SumDigit (int x)
+{
+    int n;
+    int sum = 0;
+    while (x != 0)
+    {
+        n = x % 10;
+        sum += n;
+        x /= 10;
+    }
+    return sum;
+}
+int num;
+Console.Write("Введите целое число: ");
+while(!int.TryParse(Console.ReadLine(), out num))
+    Console.Write("Ошибка ввода. Введите целое число: ");
+
+int sd = SumDigit (num);
+Console.Write ($"{num} -> {sd} ");
