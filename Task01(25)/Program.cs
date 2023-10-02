ab67b019d9c0135855d1296e3a5d93dd523829fe
Wall-e <you@mail.com>
2023-10-02 21:24:51 +0300

    Решение задачи 25\

---
+++ Task01(25)/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Ввод: числа "а" и "b"
+Вывод: "а" в натуральной степени "b" */
+
+
+bool NatureNum (int y)
+{
+    if (y > 0) return true;
+    return false;
+}
+
+int Stepen (int x, int y)
+{
+    int degree = 1;
+    for (int i = 1; i <= y; i++)
+    {
+        degree *= x;
+    }
+    return degree;
+}
+
+int a, b;
+Console.Write ("Введите целое число a: ");
+while (!int.TryParse (Console.ReadLine(), out a))
+    Console.Write ("Ошибка ввода. Введите целое число: ");
+
+EnterB:
+Console.Write ("Введите целое число b: ");
+while (!int.TryParse (Console.ReadLine(), out b))
+    Console.Write ("Ошибка ввода. Введите целое число: ");
+
+bool nn = NatureNum (b);
+int s = Stepen (a, b);
+if (nn == true)
+{
+    Console.Write ($"{a}, {b} -> {s} ");
+}
+else
+{
+    Console.WriteLine ($"Ошибка ввода. {b} не является натуральным числом");
+    goto EnterB;
+}
+
+
