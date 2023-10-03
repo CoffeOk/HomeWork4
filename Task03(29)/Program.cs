0b05d2accf3c293591258f7e7f831c53bca9a8f5
Wall-e <you@mail.com>
2023-10-03 19:52:22 +0300

    Решение задачи 29

---
+++ Task03(29)/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вывод массива из 8 элементов
+
+
+int[] RandomAray (int min, int max, int length)
+{
+    int[] array = new int[length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next (min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    string arr = string.Join(",", array);
+    for (int i = 0; i < arr.Length; i++)
+    {        
+        Console.Write ($"{arr[i]}");
+    }
+}
+
+int[] arr = RandomAray (0, 1000 , 8);
+PrintArray (arr);
+Console.Write ("-> [");
+PrintArray (arr);
+Console.Write ("]");
+
+    
